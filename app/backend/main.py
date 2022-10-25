09dd86c5ddddb96f1f734443affb59eaf7ccb395
alissonrgs <alissonrgs1993@gmail.com>
2022-10-25 09:43:59 -0300

    Fix env variables

--- app/backend/main.py
+++ app/backend/main.py
@@ -6,7 +6,7 @@ from mangum import Mangum
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # project
-from . import settings
+import settings
 
 # settings
 app = FastAPI()
@@ -25,8 +25,8 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup_db_client():
-    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
-    app.mongodb = app.mongodb_client[settings.DB_NAME]
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongodb = app.mongodb_client[settings.MONGODB_NAME]
 
 
 @app.on_event("shutdown")
